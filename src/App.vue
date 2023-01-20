b2a4dd967780ba16d9e3023491b439731a1066ee
Matthieu <reungoat.matthieu@gmail.com>
2023-01-20 22:32:51 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/destination">Destination</RouterLink>
+        <RouterLink to="/Crew">Crew</RouterLink>
+        <RouterLink to="/Technology">Technology</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
