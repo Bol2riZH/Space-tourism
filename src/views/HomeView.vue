01f53496d6603542ff61e783904e28ca08f635dc
Matthieu <reungoat.matthieu@gmail.com>
2023-01-21 23:07:29 +0100

    add home view section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,14 +9,14 @@ export default defineComponent({
 
 <template>
   <main>
-    <section class="home">
+    <section class="section section__home">
       <h2 class="subHeading-1">So, you want to travel to</h2>
       <h1 class="heading-1">Space</h1>
-      <p class="home__paragraphe">
+      <p class="section__home--paragraphe">
         Let’s face it; if you want to go to space, you might as well genuinely
         go to outer space and not hover kind of on the edge of it. Well sit
         back, and relax because we’ll give you a truly out of this world
-        experience! Explore
+        experience!
       </p>
       <Button />
     </section>
