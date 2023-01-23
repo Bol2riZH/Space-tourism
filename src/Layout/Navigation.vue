261198bcf3b8515f2bf678970bd1a8635a7603d1
Matthieu <reungoat.matthieu@gmail.com>
2023-01-23 18:01:56 +0100

    toggle between menu and nav links

--- src/Layout/Navigation.vue
+++ src/Layout/Navigation.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
-import BurgerMenu from '@/component/BurgerMenu.vue';
 import { ref } from 'vue';
-const collapse = ref(true);
+
+import BurgerMenu from '@/component/BurgerMenu.vue';
+import NavigationLinks from '@/component/NavigationLinks.vue';
+
+const toggleMenu = ref(true);
+const onToggleMenu = () => {
+  toggleMenu.value = !toggleMenu.value;
+};
 </script>
 
 <template>
   <nav class="navigation">
-    <BurgerMenu @click="collapse = !collapse" />
-    <div class="navigation__links" v-if="!collapse">
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/destination">Destination</RouterLink>
-      <RouterLink to="/crew">Crew</RouterLink>
-      <RouterLink to="/technology">Technology</RouterLink>
-    </div>
+    <BurgerMenu @click="onToggleMenu" v-if="toggleMenu" />
+    <NavigationLinks v-else :onToggleMenu="onToggleMenu" />
   </nav>
 </template>
---
+++ src/component/NavigationLinks.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+defineProps(['onToggleMenu']);
+</script>
+
+<template>
+  <ul class="navigation__links--close-cross" @click="onToggleMenu">
+    <li class="navigation__links--item">
+      <img
+        src="src/assets/image/shared/icon-close.svg"
+        alt="close navigation menu"
+      />
+    </li>
+    <li class="navigation__links--item">
+      <RouterLink to="/">Home</RouterLink>
+    </li>
+    <li class="navigation__links--item">
+      <RouterLink to="/destination">Destination</RouterLink>
+    </li>
+    <li class="navigation__links--item">
+      <RouterLink to="/crew">Crew</RouterLink>
+    </li>
+    <li class="navigation__links--item">
+      <RouterLink to="/technology">Technology</RouterLink>
+    </li>
+  </ul>
+</template>
