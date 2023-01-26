80f55bb1fcad91fa995bdce1e936786c96fd051a
Matthieu <reungoat.matthieu@gmail.com>
2023-01-26 12:03:16 +0100

    get route.params.name on watcher and change background on reactive variable

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,30 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
+import { RouterView, useRoute } from 'vue-router';
+import { watch, reactive } from 'vue';
 import Header from '@/Layout/Header.vue';
+
+const route = useRoute();
+const background = reactive({
+  url: 'src/assets/image/home/background-home-mobile.jpg',
+  size: 'cover',
+});
+
+const concatBackgroundOption = (pageName: string | string[]): any => {
+  return `no-repeat url('src/assets/image/${pageName}/background-${pageName}-mobile.jpg')`;
+};
+
+watch(route, () => {
+  let pageName: string | string[];
+
+  route.params.name === undefined
+    ? (pageName = 'home')
+    : (pageName = route.params.name);
+
+  background.url = concatBackgroundOption(pageName);
+
+  document.body.style.background = background.url;
+  document.body.style.backgroundSize = background.size;
+});
 </script>
 
 <template>
