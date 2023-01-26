9d693ab78f67644ca7f3f327456bbe5cf25f3108
Matthieu <reungoat.matthieu@gmail.com>
2023-01-26 16:03:42 +0100

    change bg > window screen size

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <script setup lang="ts">
-import { RouterView, useRoute } from 'vue-router';
 import { watch, reactive } from 'vue';
+import { RouterView, useRoute } from 'vue-router';
+
 import Header from '@/Layout/Header.vue';
 
 const route = useRoute();
+
+const tablet: number = 768;
+const desktop: number = 1440;
+
+const screen = reactive({
+  width:
+    (window.screen.width <= tablet && 'mobile') ||
+    (window.screen.width <= desktop && 'tablet') ||
+    (window.screen.width > desktop && 'desktop'),
+});
+
 const background = reactive({
   url: 'src/assets/image/home/background-home-mobile.jpg',
   size: 'cover',
+  width: screen.width,
 });
 
-const concatBackgroundOption = (pageName: string | string[]): any => {
-  return `no-repeat url('src/assets/image/${pageName}/background-${pageName}-mobile.jpg')`;
+const concatBackgroundUrlOption = (
+  pageName: string | string[],
+  format: string | boolean
+): any => {
+  return `no-repeat url('src/assets/image/${pageName}/background-${pageName}-${format}.jpg')`;
 };
 
 watch(route, () => {
@@ -20,8 +36,7 @@ watch(route, () => {
     ? (pageName = 'home')
     : (pageName = route.params.name);
 
-  background.url = concatBackgroundOption(pageName);
-
+  background.url = concatBackgroundUrlOption(pageName, screen.width);
   document.body.style.background = background.url;
   document.body.style.backgroundSize = background.size;
 });
