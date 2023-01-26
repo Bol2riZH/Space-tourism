952c26580b2c4721d94cd41e7ddda3201267edd2
Matthieu <reungoat.matthieu@gmail.com>
2023-01-26 12:36:07 +0100

    add vueUse package > close nav onClickOutside

--- src/Layout/Navigation.vue
+++ src/Layout/Navigation.vue
@@ -1,20 +1,30 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import { onClickOutside } from '@vueuse/core';
 
 import BurgerMenu from '@/component/BurgerMenu.vue';
 import NavigationLinks from '@/component/NavigationLinks.vue';
 
+const inner_nav = ref(null);
 const toggleMenu = ref(true);
 const onToggleMenu = () => {
   toggleMenu.value = !toggleMenu.value;
 };
+
+onClickOutside(inner_nav, () => {
+  toggleMenu.value = !toggleMenu.value;
+});
 </script>
 
 <template>
   <nav class="navigation">
     <BurgerMenu @click="onToggleMenu" v-if="toggleMenu" />
     <Transition>
-      <NavigationLinks v-if="!toggleMenu" :onToggleMenu="onToggleMenu" />
+      <NavigationLinks
+        ref="inner_nav"
+        v-if="!toggleMenu"
+        :onToggleMenu="onToggleMenu"
+      />
     </Transition>
   </nav>
 </template>
